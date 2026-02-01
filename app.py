6523cc499a305a3375eae9b5ec803e22f32c400b
Krish Zade <krishzade16@gmail.com>
2026-02-01 15:42:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,6 @@ from PyPDF2 import PdfReader
 GEMINI_API_KEY = "AIzaSyCELoqmwfAEnM9iqHVeTckw2f4M47LXCHs" 
 genai.configure(api_key=GEMINI_API_KEY)
 st.set_page_config(page_title="Acharya Dhanvantre Summarizer", layout="centered")
+st.title("📄 Smart Document Summarizer")
+st.subheader("Team Acharya Dhanvantre")
+st.write("Upload a PDF and get AI-powered insights in seconds.")
