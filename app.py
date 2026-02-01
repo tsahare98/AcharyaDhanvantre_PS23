d0f889243d6d5eb3b976f6ced4acfe0ee3213333
Tanmay Manoj Sahare <tsahare98@gmail.com>
2026-02-01 15:37:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,6 @@ import google.generativeai as genai
 from PyPDF2 import PdfReader
 
 
-GEMINI_API_KEY = "AIzaSyCELoqmwfAEnM9iqHVeTckw2f4M47LXCHs" # 1. Get key from Google AI Studio
+GEMINI_API_KEY = "AIzaSyCELoqmwfAEnM9iqHVeTckw2f4M47LXCHs" 
 genai.configure(api_key=GEMINI_API_KEY)
+st.set_page_config(page_title="Acharya Dhanvantre Summarizer", layout="centered")
